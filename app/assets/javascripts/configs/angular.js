12506b0499abf779d05040f641d8de13d081467d
Robert Sosinski <robert.sosinski@reactive.io>
2014-07-24 23:29:47 +0000

    getting into resources now

--- app/assets/javascripts/configs/angular.js
+++ app/assets/javascripts/configs/angular.js
@@ -3,6 +3,9 @@
 (function() {
   'use strict';
 
+  angular.module('rails', []);
+  angular.module('resources', []);
+  angular.module('directives', []);
   angular.module('controllers', []);
 })();
 
